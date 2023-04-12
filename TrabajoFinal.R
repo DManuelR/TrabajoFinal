2178de17d1f7da99f0be7d6a6cf02127a49c04f4
DManuelR <davidvillaresi@gmail.com>
2023-04-12 12:47:02 +0200

    Update TrabajoFinal.R

--- TrabajoFinal.R
+++ TrabajoFinal.R
@@ -1,15 +1,27 @@
+## Regresión logistica simple para empezar, y luego vemos si añadimos más variables (multiple), o binaria, analisis de conglomerados?
 ## 1 es blade, 0 es flake
 
 library(plyr)
-BaseDatosFinal$`Hecho sobre:` <- mapvalues(BaseDatosFinal$`Hecho sobre:`, from = c("blade", "flake"), to = c(1,0))
-
-## Creamos un vector sin la columna Hecho sobre
-
-BDatos_Final_2 <- data.frame(BaseDatosFinal$Anchura, BaseDatosFinal$Grosor, BaseDatosFinal$Longitud, BaseDatosFinal$Peso)
-BDatos_Final_2
+BaseDatosFinal$Tipo <- mapvalues(BaseDatosFinal$Tipo, from = c("blade", "flake"), to = c(1,0))
 
 ## Las variables más asociadas son la longitud con el peso, los tres asteriscos indican que es más significativa la correlación.
 
 library(correlation)
-resultados <- correlation(BDatos_Final_2)
+resultados <- correlation(BaseDatosFinal)
 resultados
+
+## utilizar glm() en vez de lm(), porque lm es gaussian y con glm puede ser binomial. Primero pasar la variable binaria a factor
+
+Tipo_1 <- as.factor(BaseDatosFinal$Tipo)
+modelo <- glm(Tipo_1 ~ BaseDatosFinal$Anchura + BaseDatosFinal$Grosor, data = BaseDatosFinal, family = binomial(link = "logit"))
+modelo
+modelo_2 <- glm(Tipo_1 ~ BaseDatosFinal$Anchura + BaseDatosFinal$Peso, data = BaseDatosFinal, family = binomial(link = "logit"))
+modelo_2
+
+## Creamos un vector con la columna tipo y grosor
+
+BDatos_Final_2 <- data.frame(BaseDatosFinal$Tipo, BaseDatosFinal$Grosor)
+BDatos_Final_2
+
+modelo_3 <- glm(Tipo_1 ~ BaseDatosFinal$Peso, data = BaseDatosFinal, family = binomial(link = "logit"))
+modelo_3
