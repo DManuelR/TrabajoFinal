05cc9c6f1c9486af0d55b5bc76cea0344890b178
DManuelR <davidvillaresi@gmail.com>
2023-06-04 11:25:41 +0200

    Update TrabajoFinal.R

--- TrabajoFinal.R
+++ TrabajoFinal.R
@@ -18,24 +18,37 @@ BaseDatosFinal$Tipo_fac
 
 # Realizamos modelos logísticos simples
 
-summary(glm(Tipo_fac ~ Anchura, BaseDatosFinal, family = "binomial"))
-summary(glm(Tipo_fac ~ Longitud, BaseDatosFinal, family = "binomial"))
-summary(glm(Tipo_fac ~ Grosor, BaseDatosFinal, family = "binomial"))
-summary(glm(Tipo_fac ~ Peso, BaseDatosFinal, family = "binomial"))
+model <- glm(Tipo_fac ~ Anchura, data = BaseDatosFinal, family = binomial())
+summary(model)
+
+model2 <- glm(Tipo_fac ~ Longitud, data = BaseDatosFinal, family = binomial())
+summary(model2)
+
+model3 <- glm(Tipo_fac ~ Grosor, data = BaseDatosFinal, family = binomial())
+summary(model3)
+
+model4 <- glm(Tipo_fac ~ Peso, data = BaseDatosFinal, family = binomial())
+summary(model4)
 
 # Con la columna Pr(>|z|) podemos ver que ninguno de los modelos simples da significativo.Podemos deducir que no existe una relación directa entre las variables y el tipo de instrumento.
 
 # Modelos logísticos multiples.
 
-summary(glm(Tipo_fac ~ Anchura + Longitud, BaseDatosFinal, family = "binomial"))
-summary(glm(Tipo_fac ~ Grosor + Peso, BaseDatosFinal, family = "binomial"))
-summary(glm(Tipo_fac ~ Anchura + Longitud + Grosor, BaseDatosFinal, family = "binomial"))
-summary(glm(Tipo_fac ~ Anchura + Longitud + Grosor + Peso, BaseDatosFinal, family = "binomial"))
-summary(glm(Tipo_fac ~ Longitud + Grosor + Peso, BaseDatosFinal, family = "binomial"))
-summary(glm(Tipo_fac ~ Longitud + Grosor, BaseDatosFinal, family = "binomial"))
-summary(glm(Tipo_fac ~ Longitud + Peso, BaseDatosFinal, family = "binomial"))
+model_1 <- glm(Tipo_fac ~ Anchura + Longitud, data = BaseDatosFinal, family = binomial())
+summary(model_1)
+model_2 <- glm(Tipo_fac ~ Grosor + Peso, data = BaseDatosFinal, family = binomial())
+summary(model_2)
+model_3 <- glm(Tipo_fac ~ Anchura + Longitud + Grosor, data = BaseDatosFinal, family = binomial())
+summary(model_3)
+model_4 <- glm(Tipo_fac ~ Anchura + Longitud + Grosor + Peso, data = BaseDatosFinal, family = binomial())
+summary(model_4)
+model_5 <- glm(Tipo_fac ~ Longitud + Grosor + Peso, data = BaseDatosFinal, family = binomial())
+summary(model_5)
+model_6 <- glm(Tipo_fac ~ Longitud + Grosor, data = BaseDatosFinal, family = binomial())
+summary(model_6)
+model_7 <- glm(Tipo_fac ~ Longitud + Peso, data = BaseDatosFinal, family = binomial())
+summary(model_7)
 
-summary(glm(Tipo_fac ~ Anchura + Longitud + Grosor + Peso, BaseDatosFinal, family = "binomial"))
 
 # Ninguna de las combinaciones probadas parecen dar resultados significativos, algunas en las que está presente la longitud y no la anchura parece que dan un poco de relación significativa pero seguramente sea por azar
 
@@ -45,12 +58,15 @@ BaseDatosFinal$Longitud_Anchura <- BaseDatosFinal$Longitud/BaseDatosFinal$Anchur
 
 # Modelo simple
 
-summary(glm(Tipo_fac ~ Longitud_Anchura, BaseDatosFinal, family = "binomial"))
+model5 <- glm(Tipo_fac ~ Longitud_Anchura, data = BaseDatosFinal, family = binomial())
+summary(model5)
 
 # Modelo multiple
 
-summary(glm(Tipo_fac ~ Longitud_Anchura + Peso, BaseDatosFinal, family = "binomial"))
-summary(glm(Tipo_fac ~ Longitud_Anchura + Grosor, BaseDatosFinal, family = "binomial"))
+model_8 <- glm(Tipo_fac ~ Longitud_Anchura + Peso, data = BaseDatosFinal, family = binomial())
+summary(model_8)
+model_9 <- glm(Tipo_fac ~ Longitud_Anchura + Grosor, data = BaseDatosFinal, family = binomial())
+summary(model_9)
 
 # Vemos que el mejor modelo es el simple (AIC más bajo y que el resto de variables no parece dar explicación del tipo de instrumento.)
 #En ese si sale significativo aunque poco, seguramente por el tamaño de la muestra. Por lo que podemos decir que existe una relación entre la proporción longitud/anchura y el tipo de instrumento.
